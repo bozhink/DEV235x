da874596decf4db87266c897e0614692f0045d85
Bozhin Karaivanov <bozhink@gmail.com>
2018-09-23 17:08:55 +0300

    Code clean-up

--- Lab_01/Lab_02/AsyncCoinWebApi/Controllers/ValuesController.cs
+++ Lab_01/Lab_02/AsyncCoinWebApi/Controllers/ValuesController.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-
-namespace AsyncCoinWebApi.Controllers
+﻿namespace AsyncCoinWebApi.Controllers
 {
+    using System.Collections.Generic;
+    using Microsoft.AspNetCore.Mvc;
+
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
